754aced1f84a3ae9c8bc00ba6f3af057ea0ede7a
Iwamgad <dagmawiharry2@gmail.com>
2023-03-07 05:08:40 -0800

    I edited some files

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -1,5 +1,5 @@
-import logo from "./holberton-logo.jpg";
-import "./App.css";
+import logo from './Holberton Logo.jpg';
+import './App.css';
 import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
@@ -9,18 +9,24 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </div>
+
       <div className="App-body">
         <p>Login to access the full dashboard</p>
         <form>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email"></input>
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password"></input>
+          <label htmlFor="email" for = "email">Email: </label>
+          <input type="email" name="email" id = "email"></input>
+          <br></br>
+          <br></br>
+          <label htmlFor="password" for = "password">Password: </label>
+          <input type="password" name="password" id ="password"></input>
+          <br></br>
+          <br></br>
           <button>OK</button>
         </form>
       </div>
+
       <div className="App-footer">
-        Copyright {getFullYear()} - {getFooterCopy()}
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
       </div>
     </div>
   );
--- 0x01-react_intro/task_3/dashboard/src/index.js
+++ 0x01-react_intro/task_3/dashboard/src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import Notifications from "./Notifications";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Notifications from "./Notifications.css"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
